ef2f07edb49863c0a7796c0a28aae738158a14ba
David Portes <daporte21@gmail.com>
2016-09-08 22:37:37 +0200

    a

--- server/dao/productCategoryDao.js
+++ server/dao/productCategoryDao.js
@@ -331,7 +331,7 @@ var productCategoryDao = {
     getCardsFromDeck : function (deckId, UserId, callback) {
 
         var connection = connectionProvider.mysqlConnectionStringProvider.getMySqlConnection();
-        var queryStatement = "SELECT * FROM bridge LEFT JOIN Cards ON bridge CardId = Cards.Id WHERE UserId = ?"
+        var queryStatement = "SELECT * FROM bridge LEFT JOIN Cards ON bridge.CardId = Cards.Id WHERE UserId = ?"
 
         if (connection) {
             connection.query(queryStatement, UserId, function (err, rows, fields) {
