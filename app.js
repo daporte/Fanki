a571365507025a114233bc38cb3070487de61e67
David Portes <daporte21@gmail.com>
2016-09-08 14:52:05 +0200

    a

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes);
 app.use('/callback', routes);
 app.use('/users', users);
 
