955f62cc3a793cd795f175347d1a0b8550b33b78
David Portes <daporte21@gmail.com>
2016-09-08 14:42:59 +0200

    a

--- setup-passport.js
+++ setup-passport.js
@@ -5,7 +5,7 @@ var strategy = new Auth0Strategy({
     domain:       'daporte21.eu.auth0.com',
     clientID:     'wdoEMhtdDrkzvnyhyOSZrDwMUqnjtEdn',
     clientSecret: 'trrfmgtYzvSYrnH3BLS1YQ2vzfZnhq_KNFDEXwgCfR6p06EtgYbDVY3Qg5uUcP-d',
-    callbackURL:  'http://fanki2.herokuapp.com/callback'
+    callbackURL:  '/callback'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
