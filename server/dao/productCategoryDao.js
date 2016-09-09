29fe68a485c13ea5c76ae2353151567c916823de
David Portes <daporte21@gmail.com>
2016-09-09 17:59:09 +0200

    a

--- server/dao/productCategoryDao.js
+++ server/dao/productCategoryDao.js
@@ -352,7 +352,7 @@ var productCategoryDao = {
     addNewCard : function (deckId, UserId, callback) {
         console.log("ADDING NEW CARD - DAO");
         var connection = connectionProvider.mysqlConnectionStringProvider.getMySqlConnection();
-        var queryStatement = "SELECT * FROM Cards Ca WHERE Decks_FK = ? AND Ca.Id NOT IN (SELECT CardId FROM bridge WHERE UserId = ? AND DeckId = ? )";
+        var queryStatement = "SELECT * FROM Cards Ca WHERE Decks_FK = ? AND Ca.Id NOT IN (SELECT CardId FROM bridge WHERE UserId = ? AND DeckId = ? ) LIMIT 1";
         //var queryStatement = "SELECT CardId FROM bridge WHERE UserId = ? AND DeckId = ? ";
         if (connection){
             connection.query(queryStatement, [deckId, UserId, deckId], function(err, rows, fields){
