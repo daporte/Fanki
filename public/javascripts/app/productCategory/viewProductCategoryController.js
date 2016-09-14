a0f4662920eae11b704a5c544efb728a7ffa17b3
David Portes <daporte21@gmail.com>
2016-09-14 22:13:33 +0200

    a

--- public/javascripts/app/productCategory/viewProductCategoryController.js
+++ public/javascripts/app/productCategory/viewProductCategoryController.js
@@ -58,11 +58,12 @@ function viewProductCategoryController($scope, $timeout, productCategoryService,
     $scope.addDetails = function(deck, on){
         console.log(on);
         loginService.storage.decks[deck.Id][1]= on ? 1:0;
-    }
+    };
 
 
     $scope.addDeck = function (deck, category, on) {
-        console.log(deck)
+        console.log(deck);
+        console.log(on);
         productCategoryService.addDeckToUser(deck, loginService.storage.UserId, on)
             .success(function (data) {
                 console.log(deck.Id)
