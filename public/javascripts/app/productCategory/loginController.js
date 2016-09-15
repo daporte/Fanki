6ac29115bb728ccb91ba53eb217e8bbd0124ea5d
David Portes <daporte21@gmail.com>
2016-09-15 23:27:38 +0200

    a

--- public/javascripts/app/productCategory/loginController.js
+++ public/javascripts/app/productCategory/loginController.js
@@ -5,12 +5,12 @@ loginController.$inject = ["$scope", "$http", "$timeout", "loginService", "requi
 
 function loginController($scope, $http, $timeout, loginService, requiredFieldValidationService_Login, $localStorage, Constants) {
 
-
+/*
     $http.get("http://fanki2.herokuapp.com/user")
         .success(function(data) {
             console.log("this is coming from wherever:" + data);
         });
-
+*/
 
     $scope.user = Constants.user;
     console.log(Constants.user);
