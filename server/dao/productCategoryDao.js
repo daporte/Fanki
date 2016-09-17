70f6c49438bb2477003c6ae628e2654edd73a55c
David Portes <daporte21@gmail.com>
2016-09-17 11:15:42 +0200

    a

--- server/dao/productCategoryDao.js
+++ server/dao/productCategoryDao.js
@@ -488,31 +488,33 @@ var productCategoryDao = {
                 console.log(rows);
                 console.log("zzzzz");
 
+                var connection2 = connectionProvider.mysqlConnectionStringProvider.getMySqlConnection();
+
+                //var queryStatement = "SELECT Decks.Id, DeckName, Description, AddedBy, IsValid, CreatedDate, ModifiedDate, CategoryId, CategoryName FROM Decks LEFT JOIN Categories ON Decks.CategoryId = Categories.Id GROUP BY Decks.CategoryId ";
+                var queryStatement = " SELECT CategoryId, CategoryName, GROUP_CONCAT(CONCAT('{\"Id\":\"', Decks.Id, '\", \"DeckName\":\"', DeckName, '\", \"Description\" : \"',Description, " +
+                    "'\" , \"AddedBy\" : \"', AddedBy, '\", \"IsValid\" : \"', IsValid, '\", \"CreatedDate\" : \"', CreatedDate, '\", \"ModifiedDate\" : \"', IFNULL(ModifiedDate, 'NULL'), '\", \"CategoryName\" : \"', IFNULL(CategoryName, 'NULL'), '\"," +
+                    "\"CategoryId\" : \"', IFNULL(CategoryId, 'NULL'), '\"}') SEPARATOR '|') as DeckList " +
+                    " FROM Decks LEFT JOIN Categories ON Decks.CategoryId = Categories.Id GROUP BY CategoryId, CategoryName;"
+
+
+                if (connection2){
+                    connection2.query(queryStatement, function(err, rows, fields){
+                        if (err) {throw err;}
+                        console.log("zzzzz");
+                        console.log(rows);
+                        console.log("zzzzz");
+                        callback(rows);
+                    });
+
+                    connectionProvider.mysqlConnectionStringProvider.closeMySqlConnection(connection2);
+                }
+                
             });
 
             connectionProvider.mysqlConnectionStringProvider.closeMySqlConnection(connection);
         }
 
-        var connection2 = connectionProvider.mysqlConnectionStringProvider.getMySqlConnection();
 
-        //var queryStatement = "SELECT Decks.Id, DeckName, Description, AddedBy, IsValid, CreatedDate, ModifiedDate, CategoryId, CategoryName FROM Decks LEFT JOIN Categories ON Decks.CategoryId = Categories.Id GROUP BY Decks.CategoryId ";
-        var queryStatement = " SELECT CategoryId, CategoryName, GROUP_CONCAT(CONCAT('{\"Id\":\"', Decks.Id, '\", \"DeckName\":\"', DeckName, '\", \"Description\" : \"',Description, " +
-            "'\" , \"AddedBy\" : \"', AddedBy, '\", \"IsValid\" : \"', IsValid, '\", \"CreatedDate\" : \"', CreatedDate, '\", \"ModifiedDate\" : \"', IFNULL(ModifiedDate, 'NULL'), '\", \"CategoryName\" : \"', IFNULL(CategoryName, 'NULL'), '\"," +
-            "\"CategoryId\" : \"', IFNULL(CategoryId, 'NULL'), '\"}') SEPARATOR '|') as DeckList " +
-            " FROM Decks LEFT JOIN Categories ON Decks.CategoryId = Categories.Id GROUP BY CategoryId, CategoryName;"
-
-
-        if (connection2){
-            connection2.query(queryStatement, function(err, rows, fields){
-                if (err) {throw err;}
-                console.log("zzzzz");
-                console.log(rows);
-                console.log("zzzzz");
-                callback(rows);
-            });
-
-            connectionProvider.mysqlConnectionStringProvider.closeMySqlConnection(connection2);
-        }
     }
 
     ,
