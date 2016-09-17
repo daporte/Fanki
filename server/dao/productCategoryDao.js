9e33b1c83eea9ce735fb394d023170af261482f5
David Portes <daporte21@gmail.com>
2016-09-17 11:16:36 +0200

    a

--- server/dao/productCategoryDao.js
+++ server/dao/productCategoryDao.js
@@ -479,7 +479,7 @@ var productCategoryDao = {
     getHierarchy : function(callback){
         var connection = connectionProvider.mysqlConnectionStringProvider.getMySqlConnection();
 
-        var setGlobal = "SET group_concat_max_len = 1000000;"
+        var setGlobal = "SET SESSION group_concat_max_len = 1000000;"
 
         if (connection){
             connection.query(setGlobal, function(err, rows, fields){
@@ -508,7 +508,7 @@ var productCategoryDao = {
 
                     connectionProvider.mysqlConnectionStringProvider.closeMySqlConnection(connection2);
                 }
-                
+
             });
 
             connectionProvider.mysqlConnectionStringProvider.closeMySqlConnection(connection);
