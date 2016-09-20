00588ca51d8f8e41b1fe5094031871db76586964
David Portes <daporte21@gmail.com>
2016-09-20 03:21:14 +0200

    a

--- public/javascripts/keystrokes.js
+++ public/javascripts/keystrokes.js
@@ -1,12 +1,36 @@
 $(function() {
-    $(document).keydown(function(e) {
-        switch (e.which) {
-            case 32:
-                $("#show").trigger("click")
-                break;
-            case 97:
-                $("#show").trigger("click")
-                break;
-        }
-    });
+        $(document).keydown(function(e) {
+            switch (e.which) {
+                case 32:
+                    $("#show").trigger("click")
+                    break;
+                case 97:
+                    $("#show").trigger("click")
+                    break;
+                case 49:
+                    $("#znovu").trigger("click")
+                    break;
+                case 97:
+                    $("#znovu").trigger("click")
+                    break;
+                case 50:
+                    $("#tezke").trigger("click")
+                    break;
+                case 98:
+                    $("#tezke").trigger("click")
+                    break;
+                case 51:
+                    $("#spravne").trigger("click")
+                    break;
+                case 99:
+                    $("#spravne").trigger("click")
+                    break;
+                case 52:
+                    $("#jednoduche").trigger("click")
+                    break;
+                case 100:
+                    $("#jednoduche").trigger("click")
+                    break;
+            }
+        });
 });
