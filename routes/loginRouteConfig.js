9666bf51610bcb0985d2fa75e3e0c29416981350
David Portes <daporte21@gmail.com>
2016-09-20 02:22:49 +0200

    a

--- routes/loginRouteConfig.js
+++ routes/loginRouteConfig.js
@@ -99,7 +99,7 @@ loginRouteConfig.prototype.addRoutes = function () {
         requestUrl : "/myDecks",
         callbackFunction : function(request, response){
             console.log("aaaaa")
-            console.log(request.app.get('bs')["_json"]["roles"]);
+            console.log(request.app.get('bs')["_json"]["roles"][0]);
             response.render("myDecks", { title : "My Decks"})
         }
 
