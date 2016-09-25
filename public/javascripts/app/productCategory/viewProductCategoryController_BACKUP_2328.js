3331b639e05bc86e9b4ad02a6c1373ccd0973642
David Portes <daporte21@gmail.com>
2016-09-25 23:36:07 +0200

    a

--- public/javascripts/app/productCategory/viewProductCategoryController_BACKUP_2328.js
+++ public/javascripts/app/productCategory/viewProductCategoryController_BACKUP_2328.js
@@ -33,10 +33,7 @@ function viewProductCategoryController($scope, $timeout, productCategoryService,
 
         if(currentCat["SubCategories"]){
             var SubCategoryIds = currentCat["SubCategories"].split("|");
-<<<<<<< HEAD
-=======
 
->>>>>>> 3bf630fa48d9a667222f5eb37b68075b9ba5bfdd
             SubCategoryIds.sort();
             for(var i=0;i<SubCategoryIds.length;i++){
 
